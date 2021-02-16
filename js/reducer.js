3bfdd743a824bbeebfeb084984039c958e0a56ff
madelinemc <madeline.coons@gmail.com>
2021-02-15 18:41:51 -0600

    commit tree 9c907f86eb4fe70977a58a276fb708501f6c5999

--- js/reducer.js
+++ js/reducer.js
@@ -1,19 +1,25 @@
-let state = {count: 0};
+let state;
 
-function changeState(state, action){
-    switch (action.type) {
-      case 'INCREASE_COUNT':
-        return {count: state.count + 1}
-      default:
-        return state;
-    }
+function changeState(state = { count: 0 }, action) {
+  switch (action.type) {
+
+    case 'INCREASE_COUNT':
+      return { count: state.count + 1 }
+
+    default:
+      return state;
   }
+}
 
 function dispatch(action){
-    state = changeState(state, action)
-    render()
+state = changeState(state, action)
+render()
 }
 
 function render(){
-    document.body.textContent = state.count
+document.body.textContent = state.count
 }
+
+
+//if we want our page to display 0 when it first loads, we can just add below at the end of the file:
+dispatch({ type: '@@INIT' })
